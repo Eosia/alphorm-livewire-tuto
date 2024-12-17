6e7dd31ac988bbc75a85e69ebbc7e0aaf3cbb651
Eosia <kevin.eosiaweb@gmail.com>
2024-12-17 08:28:03 +0100

    Chapitre 6 vidéo

--- app/Livewire/CreateUser.php
+++ app/Livewire/CreateUser.php
@@ -3,22 +3,28 @@
 namespace App\Livewire;
 
 use App\Models\User;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class CreateUser extends Component
 {
 
+    #[Rule('required|min:3')]
     public string $name;
+    #[Rule('required|min:3|email')]
     public string $email;
+    #[Rule('required|min:8')]
     public string $password;
 
     public function save() {
 
-        User::create($this->validate([
-            'name' => 'required',
-            'email' => 'required|email|unique:users,email',
-            'password' => 'required|min:8',
-        ]));
+        User::create($this->validate());
+
+//        User::create($this->validate([
+//            'name' => 'required',
+//            'email' => 'required|email|unique:users,email',
+//            'password' => 'required|min:8',
+//        ]));
 
 //        User::create([
 //            'name' => $this->name,
